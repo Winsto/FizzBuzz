6ed8c0f7b3e6d3f4404a54afd35e83637445aacb
Neil Winstanley <winsto@live.co.uk>
2015-04-18 18:29:53 +0100

    One liner (sort of) in a console app. Would really be a one liner in scriptcs

---
+++ FizzBuzzOneLiner/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FizzBuzzOneLiner
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 1; n < 101; n++){ Console.WriteLine("{0} -> {1}", n, n % 15 == 0 ? "FizzBuzz" : n % 5 == 0 ? "Buzz" : n % 3 == 0 ? "Fizz" : n.ToString()); }
+        }
+    }
+}
