324323bba5901be01f77eadb652ba3b643dd16bc
Neil Winstanley <winsto@live.co.uk>
2015-04-19 03:28:02 +0100

    Have a go at using some LINQ for this.

---
+++ FizzBuzzLINQOne/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace FizzBuzzLINQOne
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputNumbers = Enumerable.Range(1, 100);
+
+            var outputCollection =
+                inputNumbers.Select(inputNumber =>
+                    {
+                        var candidateString =
+                            DivisorStringMaps().First(mapEntry => inputNumber % mapEntry.Item1 == 0).Item2;
+
+                        return String.Format(@"{0} -> {1}",
+                                             inputNumber,
+                                             candidateString.Length == 0 ? inputNumber.ToString() : candidateString);
+                    });
+
+            foreach (var output in outputCollection) { Console.WriteLine(output); }
+        }
+
+        static IEnumerable<Tuple<int, string>> DivisorStringMaps()
+        {
+            yield return Tuple.Create(15, "FizzBuzz");
+            yield return Tuple.Create(5, "Buzz");
+            yield return Tuple.Create(3, "Fizz");
+            yield return Tuple.Create(1, "");
+        }
+    }
+}
