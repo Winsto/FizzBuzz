14e4a8cb16459d39646d7c7097d1109ea886b042
Neil Winstanley <winsto@live.co.uk>
2015-04-19 03:49:14 +0100

    Can do this instead of having the final map entry as the indicator of a mismatch

--- FizzBuzzLINQOne/Program.cs
+++ FizzBuzzLINQOne/Program.cs
@@ -13,12 +13,12 @@
             var outputCollection =
                 inputNumbers.Select(inputNumber =>
                     {
-                        var candidateString =
-                            DivisorStringMaps().First(mapEntry => inputNumber % mapEntry.Item1 == 0).Item2;
+                        var divisorStringMapEntry =
+                            DivisorStringMaps().FirstOrDefault(mapEntry => inputNumber % mapEntry.Item1 == 0);
 
                         return String.Format(@"{0} -> {1}",
                                              inputNumber,
-                                             candidateString.Length == 0 ? inputNumber.ToString() : candidateString);
+                                             divisorStringMapEntry == default(Tuple<int, string>) ? inputNumber.ToString() : divisorStringMapEntry.Item2);
                     });
 
             foreach (var output in outputCollection) { Console.WriteLine(output); }
@@ -29,7 +29,6 @@
             yield return Tuple.Create(15, "FizzBuzz");
             yield return Tuple.Create(5, "Buzz");
             yield return Tuple.Create(3, "Fizz");
-            yield return Tuple.Create(1, "");
         }
     }
 }
